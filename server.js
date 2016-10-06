5209d1f12fcc0e513c3b6b24519b2b9824056377
Mihail Puica <mihail@mpdeveloper.ca>
2016-10-05 22:07:20 -0400

    fix deprecated sendFile dynamic path

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ var express = require('express');
 
 var app     = express();
 var port    = process.env.PORT || 8081;
+
+app.use(express.static(__dirname + '/public'));
+
 // application ===================
 app.get('*', function(req, res) {
-    res.sendFile('./public/index.html');
+    res.sendFile(path.join(__dirname, './public', 'index.html'));
 });
 
 // listen ========================
