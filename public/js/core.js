7558532a34b389207c5b2a0b8a55020242a19d6a
Mihail Puica <mihail@mpdeveloper.ca>
2016-10-07 11:38:17 -0400

    frontend work - signin with upload image works - UNSTABLE

--- public/js/core.js
+++ public/js/core.js
@@ -2,9 +2,36 @@
 (function(angular){
     'use strict';
 
-    var kittyTalk = angular.module('kittyTalk', []);
+    var kittyTalk = angular.module('kittyTalk', ['ngFileUpload']);
 
-    kittyTalk.controller('ktController',function ($scope, $http) {
-    });
+    kittyTalk.controller('ktController',['$scope', 'Upload', '$timeout' ,function ($scope, Upload, $timeout, $http) {
+        $scope.kitty = {
+            id : null,
+            image : 'img/paw.jpg'
+        };
+
+        $scope.addMeowData = {};
+
+        $scope.findKittyPage = function () {
+           //todo:
+        };
+
+        $scope.signIn = function(file) {
+            file.upload = Upload.upload({
+                url: '/signin',
+                data: {username: $scope.username, file: file}
+            });
+
+            file.upload.then(function (response) {
+                $timeout(function () {
+                    // @todo: upload worked, use the response to generate Kitty page
+                });
+            }, function (response) {
+                console.log(response);
+                if (response.status > 0)
+                    $scope.errorMsg = response.status + ': ' + response.data;
+            });
+        }
+    }]);
 
 })(window.angular);
