21b6906bed539e9fbf4afa61b53e498a434691c0
Mihail Puica <mihail@mpdeveloper.ca>
2016-10-07 11:56:59 -0400

    add production db for heroku app

--- config/config.js
+++ config/config.js
@@ -2,7 +2,8 @@
 // =============
 module.exports = {
     mongourl: {
-        development: 'mongodb://mpuica:aaaaaaaa@ds049486.mlab.com:49486/kitty-talk',
+        production: 'mongodb://mpuica:aaaaaaaa@ds049486.mlab.com:49486/kitty-talk',
+        development: 'mongodb://mpuica:aaaaaaaa@ds053206.mlab.com:53206/kitty-talk-dev',
         test: 'mongodb://mpuica:aaaaaaaa@ds053206.mlab.com:53206/kitty-talk-test'
     }
 };
